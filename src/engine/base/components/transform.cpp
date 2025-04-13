1c898be76a15d5019922b780222f65ce76dc6025
Rafael Aragao Morais <ram.morais50@gmail.com>
2025-04-13 12:59:39 +0900

    Create transform component
    Signed-off-by: Rafael Aragao Morais <ram.morais50@gmail.com>

--- src/engine/base/components/transform.cpp
+++ src/engine/base/components/transform.cpp
@@ -1,1 +1,14 @@
 #include "transform.h"
+btVector3 Transform::getPosition() const { return transform_->getOrigin(); }
+btQuaternion Transform::getRotation() const {
+  return transform_->getRotation();
+}
+btVector3 Transform::getForward() const {
+  return transform_->getBasis().getColumn(2);
+}
+btVector3 Transform::getRight() const {
+  return transform_->getBasis().getColumn(0);
+}
+btVector3 Transform::getUp() const {
+  return transform_->getBasis().getColumn(1);
+}
--- src/engine/base/components/transform.h
+++ src/engine/base/components/transform.h
@@ -1,5 +1,16 @@
 #pragma once
+#include "LinearMath/btQuaternion.h"
+#include "LinearMath/btTransform.h"
+#include "LinearMath/btVector3.h"
 
+class Transform {
+ public:
+  btVector3 getPosition() const;
+  btQuaternion getRotation() const;
+  btVector3 getForward() const;
+  btVector3 getRight() const;
+  btVector3 getUp() const;
 
-class transform {
+ private:
+  btTransform* transform_;
 };
