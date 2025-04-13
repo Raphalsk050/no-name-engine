1322261e6e04115fc43526330c6ad88710d1acb6
rafael aragão morais <rafaelaragaomorais@rafa-mac.local>
2025-04-13 11:32:25 +0900

    Add build_project.sh to build, clean and run the project
    Signed-off-by: rafael aragão morais <rafaelaragaomorais@rafa-mac.local>

---
+++ build_project.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Colors
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+R="\033[0m"
+
+# Configurations
+ECC=-DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+PMV=-DCMAKE_POLICY_VERSION_MINIMUM=3.5
+BUILD_DIRECTORY=build
+BINARY="./$BUILD_DIRECTORY/main"
+
+# Functions
+build() {
+    echo -e "${YELLOW}Running CMake and generating files in $BUILD_DIRECTORY...${R}"
+    sleep 3
+    cmake -B $BUILD_DIRECTORY $ECC $PMV
+
+    if [ $? -eq 0 ]; then
+        echo -e "${GREEN}CMake finished successfully.${R}"
+        sleep 2
+        echo -e "${YELLOW}Running make to build the project...${R}"
+        sleep 3
+        cd $BUILD_DIRECTORY && make
+    else
+        echo -e "${RED}Error while running CMake.${R}"
+        exit 1
+    fi
+}
+
+clean() {
+    echo -e "${YELLOW}Cleaning $BUILD_DIRECTORY directory...${R}"
+    sleep 2
+    rm -rf $BUILD_DIRECTORY
+    echo -e "${GREEN}Build directory removed.${R}"
+}
+
+run() {
+    if [ ! -f "$BINARY" ]; then
+        echo -e "${RED}Binary not found. Please build the project first.${R}"
+        exit 1
+    fi
+    echo -e "${YELLOW}Running the application...${R}"
+    $BINARY
+}
+
+help() {
+    echo -e "${YELLOW}Usage:${R} ./build.sh {build|clean|run|help}"
+    echo
+    echo "  build   Build the project using CMake and Make"
+    echo "  clean   Remove the build directory"
+    echo "  run     Run the compiled binary"
+    echo "  help    Show this help message"
+}
+
+# Main entry point
+case "$1" in
+    build) build ;;
+    clean) clean ;;
+    run) run ;;
+    help | *) help ;;
+esac
